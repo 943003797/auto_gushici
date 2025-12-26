2187b998e85336b93658af00713516991bf4df3a
Kinson <943003797@qq.com>
2025-12-26 10:30:51 +0800

    feat: 添加环境变量支持并更新参考音频文件
    使用dotenv加载环境变量来保护API密钥
    更新参考音频文件路径并添加新的音频文件

--- index_tts2_create.py
+++ index_tts2_create.py
@@ -1,17 +1,21 @@
 import requests, base64
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 payload = {
     "model": "IndexTeam/IndexTTS-2",
-    "input": "大鹏一日同风起",
+    "input": "大鹏一日同风起.",
     "max_tokens": 2048,
-    "references": [{"audio": "data:audio/wav;base64," + base64.b64encode(open("2.mp3", "rb").read()).decode()}],
+    "references": [{"audio": "data:audio/wav;base64," + base64.b64encode(open("material/reference_audio/ttsmaker-风吟-x1.0.wav", "rb").read()).decode()}],
     "response_format": "mp3",
     "sample_rate": 32000,
     "stream": True,
     "speed": 1,
     "gain": 0
 }
-headers = {"Authorization": "Bearer sk-kvexcgafqqjhfuhnwayfrtmcyuolmoaazrqelenmhkgaaazg","Content-Type": "application/json"}
+headers = {"Authorization": "Bearer " + os.getenv("INDEXTTS_KEY"),"Content-Type": "application/json"}
 
 response = requests.post("https://api.siliconflow.cn/v1/audio/speech", json=payload, headers=headers)
 with open("1.mp3", "wb") as f:
